bcd8222a65e2b34afd09dd2c9b6ab14036e95de1
vitamin777 <rlaalsdn8@gmail.com>
2023-02-25 18:06:20 +0900

    feat: class 와 method 를 생성해 호출 테스트 적용
    -1- index.js 에서 class Player 를 생성해 해당 클래스안에 firstMeet method 작성하여
    
    -2- 클래스 외부에서 Player 클래스의 firstMeet method 를 호출해 console 출력 확인

--- dist/index.js
+++ dist/index.js
@@ -1,2 +1,14 @@
 "use strict";
-console.log('index.ts');
+
+class Player {
+	firstMeet(){
+		console.log("hello world!")
+	}
+}
+
+const player1 = new Player()
+player1.firstMeet()
+console.log(player1)
+const player2 = new Player()
+player2.firstMeet()
+console.log(player2)
