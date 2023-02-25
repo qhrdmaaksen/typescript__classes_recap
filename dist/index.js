e6d905c645667d5d17297312cc9286ac2d64923a
vitamin777 <rlaalsdn8@gmail.com>
2023-02-25 18:24:41 +0900

    feat: 클래스 필드 정의하여 해당 값을 활용해 life 동적 변경 테스트 적용
    -1- Player class 안에 constructor 밖에 field score, numLife 설정
    
    -2- loseLife 함수를 class 내에 생성해 호출될 시 numLife 값에서 -1 되도록 함
    
    -3- 클래스 밖에서 player1 의 초기 life 10 값 콘솔 확인
    
    -3-02- player1 에 loseLife 함수 호출하고 다시 player life 를 콘솔 확인 결과 최초 10 에서 9 로 변경됨을 확인

--- dist/index.js
+++ dist/index.js
@@ -1,14 +1,31 @@
-"use strict";
+'use strict';
 
 class Player {
-	firstMeet(){
-		console.log("hello world!")
+	score = 0 ;
+	numLife = 10;
+  constructor(first, last) {
+    console.log('in constructor');
+		this.first = first;
+		this.last = last;
+  }
+  firstMeet() {
+    console.log('hello world!');
+  }
+	loseLife() {
+		this.numLife -= 1;
 	}
 }
 
-const player1 = new Player()
-player1.firstMeet()
-console.log(player1)
-const player2 = new Player()
-player2.firstMeet()
-console.log(player2)
+const player1 = new Player("kim","minwoo");
+player1.firstMeet();
+console.log(player1.first)
+console.log(player1.last)
+console.log(player1.numLife)
+player1.loseLife()
+console.log(player1.numLife)
+console.log(Player)
+const player2 = new Player("shin","hyunwoo");
+player2.firstMeet();
+console.log(player2.first)
+console.log(player2.last)
+console.log(player2);
