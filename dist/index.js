20140431302ae50fa5768019a58ceef95b78edd9
vitamin777 <rlaalsdn8@gmail.com>
2023-02-26 16:34:42 +0900

    feat: getter,setter 활용해 score&fullName 불러오기 및 업데이트 테스트 적용
    -1- fullName 을 가져오기 위해 get fullName method 작성 (성과, 이름 반환토록함)
    -1-02- class 밖에서 player1 의 full name 조회 성공
    
    -2-01- get score method 작성 (음수일 경우 에러 출력되도록함)
    -2-02- 클래스 밖에서 player1.score = 숫자 대입 후 player1.score 조회 결과 출력 확인
    
    -3- 클래스 내에서 setter set fullName method 작성(인자로 newName 설정) 성과 이름의 공백 제거 후 first,last 배열 타입으로 담도록하고 생성자 함수의 first 에 first, last 에 last 대입토록함
    -3-02- player1.fullName 으로 조회해 기존 name 확인 후 player1.fullName='이름' 기입해 콘솔창에 정상 결과 출력 확인

--- dist/index.js
+++ dist/index.js
@@ -1,43 +1,68 @@
 'use strict';
 
 class Player {
-	#score = 0 ;
-	numLife = 10;
+  #score = 0;
+  numLife = 10;
   constructor(first, last) {
     console.log('in constructor');
-		this.first = first;
-		this.last = last;
-		this.#secret();
+    this.first = first;
+    this.last = last;
+    this.#secret();
   }
-	getScore() {
-		return this.#score;
-	}
-	setScore (newScore) {
-		return this.#score = newScore;
-	}
+  get fullName() {
+    return `${this.first} ${this.last}`;
+  }
+  set fullName(newName) {
+    const [first, last] = newName.split(' ');
+    this.first = first;
+    this.last = last;
+  }
+  get score() {
+    return this.#score;
+  }
+  set score(newScore) {
+    if (newScore < 0) {
+      throw new Error('this is not positive number');
+    }
+    this.#score = newScore;
+  }
+  getScore() {
+    return this.#score;
+  }
+  setScore(newScore) {
+    return (this.#score = newScore);
+  }
   firstMeet() {
     console.log('hello world!');
   }
-	loseLife() {
-		this.numLife -= 1;
-	}
-	#secret() {
-		console.log('secret')
-	}
+  loseLife() {
+    this.numLife -= 1;
+  }
+  #secret() {
+    console.log('secret');
+  }
 }
 
-const player1 = new Player("kim","minwoo");
+const player1 = new Player('kim', 'minwoo');
 player1.firstMeet();
+console.log(player1.first);
+console.log(player1.last);
+console.log(player1.numLife);
+player1.loseLife();
+console.log(player1.numLife);
+console.log(player1.getScore());
+console.log(player1.setScore(28));
+console.log(player1.fullName);
+console.log(player1.score);
+player1.score = 20;
+console.log(player1.score);
+player1.fullName = 'kim woobin';
+console.log(player1.fullName)
 console.log(player1.first)
 console.log(player1.last)
-console.log(player1.numLife)
-player1.loseLife()
-console.log(player1.numLife)
-console.log(player1.getScore())
-console.log(player1.setScore(28))
-console.log(Player)
-const player2 = new Player("shin","hyunwoo");
+console.log(Player);
+const player2 = new Player('shin', 'hyunwoo');
 player2.firstMeet();
-console.log(player2.first)
-console.log(player2.last)
+console.log(player2.first);
+console.log(player2.last);
 console.log(player2);
