c050b72d9e4cac67e435b126ce468a8cc6069398
vitamin777 <rlaalsdn8@gmail.com>
2023-02-26 21:35:16 +0900

    feat: typescript 내의 클래스 필드를 다뤘으며 타입 추론 토록 설정함
    -1- index.ts 에서 class field 로 first,last,score 에대한 타입 정의 및
    클래스 밖에서 vitamin 에 뉴 클래스로 성, 이름 을 인자로 넣어주고 score 에 점수 대입 후 콘솔 확인 정상 출력

--- src/dist/index.ts
+++ src/dist/index.ts
@@ -1,1 +1,14 @@
-console.log('index.ts')
+class Player {
+	// 아래 first & last 는 typescript 에게 type 을 알려주는 것임(타입 추론)
+	first: string;
+	last: string;
+	score: number = 0;
+	constructor(first: string, last: string) {
+		this.first = first;
+		this.last = last;
+	}
+}
+
+const vitamin = new Player('vita', 'min');
+vitamin.score = 33;
+console.log(vitamin);
