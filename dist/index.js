a46da890bee6ce1aef3ca4bc1711be9824fd5383
vitamin777 <rlaalsdn8@gmail.com>
2023-02-27 22:14:34 +0900

    feat: 단축 구문활용해 first, last, score code 단축 & typescript 에서 getter, setter 활용해 값을 불러오고 쓰도록 설정
    -1- Player 클래스의 생성자 함수에 인자로 first,last, _score 에대해 타입 정의 및 public 설정하여 클래스 필드에 적었던 코드를 줄이고 생성자 함수에 초기화 코드또한 필요 없어져 간결한 코드작성함
    
    -2- method 앞에 get, set 을 사용하고 각 타입 지정을 해줬으며 get method 설정했기에 Player 클래스의 메서드에 접근할수있고 set method 로 설정했기에 접근해 score 의 값을 변경할 수 있도록하였음
    
    (정상 적용 및 출력확인)

--- dist/index.js
+++ dist/index.js
@@ -1,16 +1,40 @@
 "use strict";
 class Player {
-    constructor(first, last) {
-        this.score = 0;
+    // 아래 first & last 는 typescript 에게 type 을 알려주는 것임(타입 추론)
+    // public 명시해주면 클래스 외부에서 프로퍼티 변경,접근,쓰기 할수있다고 알리는것
+    /* 단축 구문으로 생성자 함수에 인자로 설정하기에 주석처리.
+    public readonly first: string;
+    public readonly last: string;
+    private score: number = 0;*/
+    constructor(first, last, _score) {
         this.first = first;
         this.last = last;
+        this._score = _score;
+        /* 단축 구문으로 생성자 함수에 인자로 설정하기에 초기화 필요없어져서 주석처리함.
+        this.first = first;
+        this.last = last;*/
     }
     // private 라고 명시해주면 해당 메서드나 프로퍼티가 정의된 함수 내부에서만 접근 가능하다고 알리는것
     secretMethode() {
         console.log('secretMethod 호출');
     }
+    get fullName() {
+        return `${this.first} ${this.last}`;
+    }
+    get score() {
+        return this._score;
+    }
+    set score(newScore) {
+        if (newScore < 0) {
+            throw new Error('점수는 0 이상이어야 합니다.');
+        }
+        this._score = newScore;
+    }
 }
-const vitamin = new Player('vita', 'min');
+const vitamin = new Player('vita', 'min', 33);
+vitamin.fullName;
+vitamin.score;
+vitamin.score = 36;
 // private score 는 클래스 외부에서 접근 불가하기에 아래 code 에서 에러 발생
 //vitamin.score = 33;
 // 아래 code 에서 vitamin.first = 'vitamin' // readonly 이기 때문에 에러 발생
--- src/dist/index.ts
+++ src/dist/index.ts
@@ -1,20 +1,40 @@
 class Player {
   // 아래 first & last 는 typescript 에게 type 을 알려주는 것임(타입 추론)
   // public 명시해주면 클래스 외부에서 프로퍼티 변경,접근,쓰기 할수있다고 알리는것
+  /* 단축 구문으로 생성자 함수에 인자로 설정하기에 주석처리.
   public readonly first: string;
   public readonly last: string;
-  private score: number = 0;
-  constructor(first: string, last: string) {
+  private score: number = 0;*/
+  constructor(public first: string, public last: string, private _score: number) {
+    /* 단축 구문으로 생성자 함수에 인자로 설정하기에 초기화 필요없어져서 주석처리함.
     this.first = first;
-    this.last = last;
+    this.last = last;*/
   }
 	// private 라고 명시해주면 해당 메서드나 프로퍼티가 정의된 함수 내부에서만 접근 가능하다고 알리는것
   private secretMethode(): void {
 		console.log('secretMethod 호출');
 	}
+
+	get fullName(): string {
+		return `${this.first} ${this.last}`;
+	}
+	get score(): number {
+		return this._score;
+	}
+	set score(newScore: number){
+		if (newScore < 0) {
+			throw new Error('점수는 0 이상이어야 합니다.');
+		}
+		this._score = newScore;
+	}
 }
 
-const vitamin = new Player('vita', 'min');
+
+
+const vitamin = new Player('vita', 'min', 33);
+vitamin.fullName;
+vitamin.score;
+vitamin.score = 36;
 // private score 는 클래스 외부에서 접근 불가하기에 아래 code 에서 에러 발생
 //vitamin.score = 33;
 // 아래 code 에서 vitamin.first = 'vitamin' // readonly 이기 때문에 에러 발생
