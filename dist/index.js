4598d294928deeb6733e8a74a5cce2e2c06c60a4
vitamin777 <rlaalsdn8@gmail.com>
2023-02-27 22:29:24 +0900

    feat: protected 를 활용해 자식 클래스에서만 필드에 접근해 score value 변경토록 함
    -1- Player 의 생성자 함수의 인자 score 를 private 에서 protected 로 변경해 자식 클래스인 SuperPlayer 클래스에서 score 의 값에 접근해 스코어 변경 가능 토록함
    
    (정상 출력 확인)

--- dist/index.js
+++ dist/index.js
@@ -31,10 +31,23 @@ class Player {
         this._score = newScore;
     }
 }
+class SuperPlayer extends Player {
+    constructor() {
+        super(...arguments);
+        this.isAdmin = true;
+    }
+    maxScore() {
+        this._score = 999;
+    }
+}
 const vitamin = new Player('vita', 'min', 33);
 vitamin.fullName;
 vitamin.score;
 vitamin.score = 36;
+const superMan = new SuperPlayer("vi", "ta", 13);
+console.log(superMan);
+console.log(superMan.maxScore());
+console.log(superMan);
 // private score 는 클래스 외부에서 접근 불가하기에 아래 code 에서 에러 발생
 //vitamin.score = 33;
 // 아래 code 에서 vitamin.first = 'vitamin' // readonly 이기 때문에 에러 발생
