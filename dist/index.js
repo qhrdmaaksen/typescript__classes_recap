73cca67bb60bb6fdce521fdbe72ce96a760b7678
vitamin777 <rlaalsdn8@gmail.com>
2023-02-27 16:43:12 +0900

    feat: js es2015 변경 & typescript 에서 public, private, readonly 사용법 테스트
    -1- index.ts 에서 class field 사용 및 type 설정 작성
    
    -2- readonly public private 를 붙여 사용해보고 적용되는지 테스트
    (정상적으로 적용됨을 확인)

--- dist/index.js
+++ dist/index.js
@@ -1,12 +1,20 @@
 "use strict";
-var Player = /** @class */ (function () {
-    function Player(first, last) {
+class Player {
+    constructor(first, last) {
         this.score = 0;
         this.first = first;
         this.last = last;
     }
-    return Player;
-}());
-var vitamin = new Player('vita', 'min');
-vitamin.score = 33;
+    // private 라고 명시해주면 해당 메서드나 프로퍼티가 정의된 함수 내부에서만 접근 가능하다고 알리는것
+    secretMethode() {
+        console.log('secretMethod 호출');
+    }
+}
+const vitamin = new Player('vita', 'min');
+// private score 는 클래스 외부에서 접근 불가하기에 아래 code 에서 에러 발생
+//vitamin.score = 33;
+// 아래 code 에서 vitamin.first = 'vitamin' // readonly 이기 때문에 에러 발생
+//vitamin.first = 'vitamin'
+// 아래 code 에서 vitamin.secretMethode() // private method 이기 때문에 에러 발생
+//vitamin.secretMethode()
 console.log(vitamin);
--- src/dist/index.ts
+++ src/dist/index.ts
@@ -1,14 +1,24 @@
 class Player {
-	// 아래 first & last 는 typescript 에게 type 을 알려주는 것임(타입 추론)
-	first: string;
-	last: string;
-	score: number = 0;
-	constructor(first: string, last: string) {
-		this.first = first;
-		this.last = last;
+  // 아래 first & last 는 typescript 에게 type 을 알려주는 것임(타입 추론)
+  // public 명시해주면 클래스 외부에서 프로퍼티 변경,접근,쓰기 할수있다고 알리는것
+  public readonly first: string;
+  public readonly last: string;
+  private score: number = 0;
+  constructor(first: string, last: string) {
+    this.first = first;
+    this.last = last;
+  }
+	// private 라고 명시해주면 해당 메서드나 프로퍼티가 정의된 함수 내부에서만 접근 가능하다고 알리는것
+  private secretMethode(): void {
+		console.log('secretMethod 호출');
 	}
 }
 
 const vitamin = new Player('vita', 'min');
-vitamin.score = 33;
+// private score 는 클래스 외부에서 접근 불가하기에 아래 code 에서 에러 발생
+//vitamin.score = 33;
+// 아래 code 에서 vitamin.first = 'vitamin' // readonly 이기 때문에 에러 발생
+//vitamin.first = 'vitamin'
+// 아래 code 에서 vitamin.secretMethode() // private method 이기 때문에 에러 발생
+//vitamin.secretMethode()
 console.log(vitamin);
